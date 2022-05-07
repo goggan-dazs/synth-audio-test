7436a90a984f3cb33dc21652c4cb65663b331425
Chris Goggans <cgogg1@gmail.com>
2022-05-07 12:58:28 -0400

    succesfully tested tonaljs synth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import * as Tone from "tone";
+
 import './App.css';
+
+
+
+const synth = new Tone.Synth().toDestination();
+      
+function playSynth() {
+  synth.triggerAttackRelease("D4", "8n")
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrapper">
+        <button id="button" onClick={playSynth}>click me</button>
+      </div>
     </div>
   );
 }
